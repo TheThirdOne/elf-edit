ae262b271183fc1da2828645491f81f73c61ab5d
Benjamin Landers <benjaminrlanders@gmail.com>
2019-01-17 07:30:35 -0800

    Bit uglier, but more readable colors

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,9 @@ fn main() {
 
   window.keypad(true);
   init_pair(1, COLOR_BLACK, COLOR_YELLOW);
-  init_pair(2, COLOR_WHITE, COLOR_GREEN);
-  init_pair(3, COLOR_WHITE, COLOR_CYAN);
-  init_pair(4, COLOR_WHITE, COLOR_BLUE);
+  init_pair(2, COLOR_BLACK, COLOR_GREEN);
+  init_pair(3, COLOR_BLACK, COLOR_CYAN);
+  init_pair(4, COLOR_BLACK, COLOR_BLUE);
   init_pair(5, COLOR_WHITE, COLOR_RED);
   init_pair(6, COLOR_WHITE, COLOR_MAGENTA);
 	
